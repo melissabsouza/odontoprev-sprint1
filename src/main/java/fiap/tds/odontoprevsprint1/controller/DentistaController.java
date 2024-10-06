39d857c7070f2bd157e81e4128bea5f0df02b558
Melissa <melissabsouzaa@gmail.com>
2024-10-06 01:24:55 -0300

    feat: dentista controller done

---
+++ src/main/java/fiap/tds/odontoprevsprint1/controller/DentistaController.java
@@ -0,0 +1,50 @@
+package fiap.tds.odontoprevsprint1.controller;
+
+import fiap.tds.odontoprevsprint1.dto.DentistaDTO;
+import fiap.tds.odontoprevsprint1.dto.PacienteDTO;
+import fiap.tds.odontoprevsprint1.service.DentistaService;
+import fiap.tds.odontoprevsprint1.service.PacienteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/dentistas")
+@AllArgsConstructor
+public class DentistaController {
+    private DentistaService dentistaService;
+
+    @GetMapping
+    public ResponseEntity<List<DentistaDTO>> getAllDentistas() {
+        List<DentistaDTO> dentistas = dentistaService.getAllDentistas();
+        return ResponseEntity.ok(dentistas);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createDentista(@RequestBody DentistaDTO dentistaDTO) {
+        try {
+            DentistaDTO novoDentista = dentistaService.saveDentista(dentistaDTO);
+            return new ResponseEntity<>(novoDentista, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>("Dentista já existente", HttpStatus.CONFLICT);
+        }
+
+    }
+
+    @GetMapping("/{cpf}")
+    public ResponseEntity<DentistaDTO> buscarPorCpf(@PathVariable Long cpf) {
+        Optional<DentistaDTO> dentistaDTO = dentistaService.buscarPorCpf(cpf);
+        return dentistaDTO.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Void> deleteDentista(@PathVariable Long cpf) {
+        dentistaService.deleteDentista(cpf);
+        return ResponseEntity.noContent().build();
+    }
+}
